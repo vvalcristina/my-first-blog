462ae9dea4eaced772e9af8b129aab6913140cdc
Valeria <silvvavaleria@gmail.com>
2019-09-16 21:45:25 -0300

    Iniciando desafio Women Can Code Python

--- blog/views.py
+++ blog/views.py
@@ -5,7 +5,6 @@ from .models import Post
 from .forms import PostForm
 from django.shortcuts import redirect
 
-
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
@@ -14,6 +13,9 @@ def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
 
+def post_new(request):
+    form= PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
 
 def post_new(request):
      if request.method == "POST":
